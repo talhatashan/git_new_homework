e29145fb046b8aacaf8119b4394b09fbe3b12352
Talha Tashan <talhatashan@gamil.com>
2023-08-29 18:38:37 +0300

    pdf convertor created

---
+++ pdf.py
@@ -0,0 +1,19 @@
+import pdf2docx
+# Importing the Converter() class
+from pdf2docx import Converter
+
+# Specifying the pdf & docx files
+pdf_file = 'sonuc.pdf'
+docx_file = 'sonuc.docx'
+
+try:
+    # Converting PDF to Docx
+    cv_obj = Converter(pdf_file)
+    cv_obj.convert(docx_file)
+    cv_obj.close()
+
+except:
+    print('Conversion Failed')
+
+else:
+    print('File Converted Successfully')
